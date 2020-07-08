cfd79a13dfac995e5b85c585f7d274c15834583e
Mark Gire <magire01@gmail.com>
2020-07-07 22:04:05 -0400

    Successfully added weather API to project and printed the object to console

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,15 @@
 console.log("test");
+
+var cityName = "Denver";
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=3e1d9b21516255772cfd52b6d808ff3c";
+
+function displayWeather() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+}
+
+displayWeather();
