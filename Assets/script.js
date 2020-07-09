403a4b4a208d42feba0a426e18faceeb922c7c14
Mark Gire <magire01@gmail.com>
2020-07-09 00:16:36 -0400

    On Click now displays a defnied city's weather info on page. Still need to make it read the search bar city name

--- Assets/script.js
+++ Assets/script.js
@@ -1,53 +1,65 @@
-console.log("test");
+$(document).ready(function() {
 
-var searchBarText = $("#search-bar")
-var searchBtn = $("#search-button");
-var cityName = ["Louisville"];
-searchBtn.on("click", function() {
-    cityName.push(searchBarText.val());
-    console.log(cityName)
-})
+    console.log("test");
 
+    var searchBarText = $("#search-bar")
+    var searchBtn = $("#search-button");
+    var cityName = "Louisville";
 
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=3e1d9b21516255772cfd52b6d808ff3c`;
 
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName[0] + "&appid=3e1d9b21516255772cfd52b6d808ff3c";
+    console.log(queryURL);
 
 
-function displayWeather() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response);
+    function weather() {
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            var nameCity = response.city.name;
+            var nameDate = response.list[0].dt_txt;
+            var nameTemp = response.list[0].main.temp;
+            var nameHumidity = response.list[0].main.humidity;
+            var nameWindSpeed = response.list[0].wind.speed;
+            var nameUVIndex = response.list[0].clouds.all;
+            searchBtn.on("click", function () {
+                var cityDiv = $("<h3>");
+                cityDiv.text(nameCity + " (" + nameDate + ")");
+                $("#search-results").append(cityDiv);
+                
+                var tempDiv = $("<h6>");
+                tempDiv.text("Temperature: " + nameTemp);
+                $("#search-results").append(tempDiv);
+
+                var humidityDiv = $("<h6>");
+                humidityDiv.text("Humidity: " + nameHumidity);
+                $("#search-results").append(humidityDiv);
+
+                var windSpeedDiv = $("<h6>");
+                windSpeedDiv.text("Wind Speed: " + nameWindSpeed);
+                $("#search-results").append(windSpeedDiv);
+
+                var UVIndexDiv = $("<h6>");
+                UVIndexDiv.text("UV Index: " + nameUVIndex);
+                $("#search-results").append(UVIndexDiv);
+                event.preventDefault();
+            });
+
+        })
         
-        var nameCity = response.city.name;
-        var nameDate = response.list[0].dt_txt;
-        var nameTemp = response.list[0].main.temp;
-        var nameHumidity = response.list[0].main.humidity;
-        var nameWindSpeed = response.list[0].wind.speed;
-        var nameUVIndex = response.list[0].clouds.all;
+    }
 
-        var cityDiv = $("<h3>");
-        cityDiv.text(nameCity + " (" + nameDate + ")");
-        $("#search-results").append(cityDiv);
-        
-        var tempDiv = $("<h6>");
-        tempDiv.text("Temperature: " + nameTemp);
-        $("#search-results").append(tempDiv);
+    // searchBtn.on("click", function() {
+    //     cityName.push(searchBarText.val());
+    //     console.log(cityName);
+    
+    // });
 
-        var humidityDiv = $("<h6>");
-        humidityDiv.text("Humidity: " + nameHumidity);
-        $("#search-results").append(humidityDiv);
+    // searchBtn.on("click", function () {
+    //     weather();
+    // });
 
-        var windSpeedDiv = $("<h6>");
-        windSpeedDiv.text("Wind Speed: " + nameWindSpeed);
-        $("#search-results").append(windSpeedDiv);
-
-        var UVIndexDiv = $("<h6>");
-        UVIndexDiv.text("UV Index: " + nameUVIndex);
-        $("#search-results").append(UVIndexDiv);
-
-    })
-}
-
-displayWeather();
+    weather();
+});
